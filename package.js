4ce7b06907f844e54fe54e5dc16edee1bb8a341a
James Rumack <james.rumack@videoplaza.com>
2014-10-21 21:54:14 +0200

    initial commit

---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+  name: 'sidr',
+  summary: 'Sidr is a jQuery plugin for creating side menus and the easiest way for doing your menu responsive. ',
+  version: '1.2.1',
+  git: 'https://github.com/fredstrange/meteor-sidr.git'
+});
+
+Package.onUse(function(api) {
+api.versionsFrom('METEOR@0.9.0');
+
+api.use(['jquery'], ['client']);;
+
+api.add_files(["sidr/dist/jquery.sidr.js", "sidr/dist/stylesheets/jquery.sidr.dark.css"], "client");
+});
+
